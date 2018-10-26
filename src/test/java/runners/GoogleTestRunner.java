1023d733408df46ef0b2b7ae3c944b631dfee475
Tupik <boris_tupik@senla.eu>
2018-10-26 15:00:31 +0300

    Updated allure reporting.

--- src/test/java/runners/GoogleTestRunner.java
+++ src/test/java/runners/GoogleTestRunner.java
@@ -6,10 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        strict = true,
         format = {"pretty", "json:target/cucumber.json", "html:target/cucumber.html"},
         features = {
                 "src/test/cucumber/",
         },
+        plugin = {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
         glue = {"steps"})
 public class GoogleTestRunner {
 }
